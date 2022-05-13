70386089e7edc06bddc1354824d7ae3d16a691a1
AlexTa <alex.tarasov.work@gmail.com>
2022-05-13 14:09:18 +0300

    show table

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  useEffect(() => {
+    console.log('useEffect')
+  const sheetId = '1-38BWWwyzw16CW5rPycbnK73MAg8MbM9';
+  const base = `https://docs.google.com/spreadsheets/d/${sheetId}/gviz/tq?`;
+  const sheetName = 'user-data';
+  const query = encodeURIComponent('Select *')
+  const url = `${base}&sheet=${sheetName}&tq=${query}`
+
+  const data = []  
+  
+  const output = document.querySelector('.output')
+  
+  function init() {
+    fetch(url)
+        .then(res => res.text())
+        .then(rep => {
+            //Remove additional text and extract only JSON:
+            const jsonData = JSON.parse(rep.substring(47).slice(0, -2));
+            console.log(jsonData)
+ 
+            const colz = [];
+            const tr = document.createElement('tr');
+            //Extract column labels
+            jsonData.table.cols.forEach((heading) => {
+                if (heading.label) {
+                    let column = heading.label;
+                    colz.push(column);
+                    const th = document.createElement('th');
+                    th.innerText = column;
+                    tr.appendChild(th);
+                }
+            })
+            output.appendChild(tr);
+ 
+            //extract row data:
+            jsonData.table.rows.forEach((rowData) => {
+                const row = {};
+                colz.forEach((ele, ind) => {
+                    row[ele] = (rowData.c[ind] != null) ? rowData.c[ind].v : '';
+                })
+                data.push(row);
+            })
+            processRows(data);
+            console.log(data)
+        })
+    }
+      
+    function processRows(json) {
+        json.forEach((row) => {
+    
+            const tr = document.createElement('tr');
+            const keys = Object.keys(row);
+        
+            keys.forEach((key) => {
+                const td = document.createElement('td');
+                td.textContent = row[key];
+                tr.appendChild(td);
+            })
+            output.appendChild(tr);
+        })
+    }
+
+    init()
+
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table className="output"></table>  
     </div>
   );
 }
